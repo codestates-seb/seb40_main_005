0fda574a06e72e26538b63376e06aea3a23d2d0b
JungSoyi <soi0125@naver.com>
2022-11-12 00:30:08 +0900

    :art: styling : board post dto 작성

--- server/src/main/java/com/gallendar/gradle/server/domain/board/controller/BoardController.java
+++ server/src/main/java/com/gallendar/gradle/server/domain/board/controller/BoardController.java
@@ -1,9 +1,20 @@
 package com.gallendar.gradle.server.domain.board.controller;
 
+import com.gallendar.gradle.server.domain.board.dto.BoardPostDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/boards")
 public class BoardController {
+
+    @PostMapping
+    public ResponseEntity postBoard(@RequestBody BoardPostDto boardDto){
+
+        return null;
+    }
 }
--- server/src/main/java/com/gallendar/gradle/server/domain/board/dto/BoardPostDto.java
+++ server/src/main/java/com/gallendar/gradle/server/domain/board/dto/BoardPostDto.java
@@ -1,4 +1,19 @@
 package com.gallendar.gradle.server.domain.board.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+
 public class BoardPostDto {
+
+    @NotBlank(message = "제목을 입력하세요.")
+    private String title; // 게시글 제목
+
+    @NotBlank(message = "내용을 입력하세요.")
+    private String content; // 게시글 본문
+
+    @NotBlank(message = "사진을 첨부하세요")
+    private MultipartFile photo;
+
+    private String music;
 }
