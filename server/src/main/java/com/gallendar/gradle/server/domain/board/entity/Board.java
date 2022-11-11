fc49011aab78adf037b2a4f669d9ea03177f6861
JungSoyi <soi0125@naver.com>
2022-11-12 00:10:31 +0900

    :art: styling : Board entity 생성

--- server/src/main/java/com/gallendar/gradle/server/domain/board/entity/Board.java
+++ server/src/main/java/com/gallendar/gradle/server/domain/board/entity/Board.java
@@ -4,13 +4,44 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Getter
 @NoArgsConstructor
 @Entity
-@Builder
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(length = 100, nullable = false)
+    private MultipartFile photo;
+
+    @Column(nullable = true)
+    private String music;
+
+    @Builder
+    public Board(Long boardId, String title, String content, MultipartFile photo, String music){
+        this.boardId = boardId;
+        this.title = title;
+        this.content = content;
+        this.photo = photo;
+        this.music = music;
+    }
+
+
+
 
 }
