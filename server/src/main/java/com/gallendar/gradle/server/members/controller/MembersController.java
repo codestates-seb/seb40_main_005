685bd63c730ec8383aabad9a43e772d0476d248e
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-11-14 04:50:50 +0900

    ✨ feat : members login

--- server/src/main/java/com/gallendar/gradle/server/members/controller/MembersController.java
+++ server/src/main/java/com/gallendar/gradle/server/members/controller/MembersController.java
@@ -1,7 +1,25 @@
 package com.gallendar.gradle.server.members.controller;
 
+import com.gallendar.gradle.server.global.auth.jwt.JwtUtils;
+import com.gallendar.gradle.server.members.dto.LoginRequest;
+import com.gallendar.gradle.server.members.dto.LoginResponse;
+import com.gallendar.gradle.server.members.service.LoginService;
+import com.gallendar.gradle.server.members.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MembersController {
+    private final LoginService loginService;
+    private final JwtUtils jwtUtils;
+    @PostMapping("/authentication")
+    public LoginResponse membersLogin(@RequestBody LoginRequest loginRequest){
+        System.out.println(loginRequest.getId());
+        return loginService.LoginMembers(loginRequest);
+    }
 }
