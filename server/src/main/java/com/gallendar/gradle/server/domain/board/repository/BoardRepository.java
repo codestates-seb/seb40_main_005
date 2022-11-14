7d28d56aa22221b18f8768e17ce601dcc2a349e1
JungSoyi <104415354+JungSoyi@users.noreply.github.com>
2022-11-14 13:15:19 +0900

    Revert "Feature/jungsoyi/board#2 (#33)"
    This reverts commit 57591d45994796704b1e8f6eeecd108a5b1f4be6.

--- server/src/main/java/com/gallendar/gradle/server/domain/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gallendar.gradle.server.domain.board.repository;
-
-import com.gallendar.gradle.server.domain.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
