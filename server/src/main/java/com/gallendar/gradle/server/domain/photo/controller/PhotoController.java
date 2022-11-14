05f5d2a76bb97727717fc43a3f34e799e05f8af5
JungSoyi <104415354+JungSoyi@users.noreply.github.com>
2022-11-14 13:29:27 +0900

    Revert "Feature/jungsoyi/board#2 (#33)" (#36)
    This reverts commit 57591d45994796704b1e8f6eeecd108a5b1f4be6.

--- server/src/main/java/com/gallendar/gradle/server/domain/photo/controller/PhotoController.java
+++
@@ -1,8 +0,0 @@
-package com.gallendar.gradle.server.domain.photo.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class PhotoController {
-
-}
--- server/src/main/java/com/gallendar/gradle/server/domain/photo/service/PhotoService.java
+++
@@ -1,8 +0,0 @@
-package com.gallendar.gradle.server.domain.photo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PhotoService {
-
-}
