893572bad065bbc12cfa3df39868dd5fe3a37897
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-11-15 17:43:03 +0900

    ✨ feat : member search by id

--- server/src/main/java/com/gallendar/gradle/server/members/controller/MembersController.java
+++ server/src/main/java/com/gallendar/gradle/server/members/controller/MembersController.java
@@ -3,22 +3,28 @@ package com.gallendar.gradle.server.members.controller;
 import com.gallendar.gradle.server.global.auth.jwt.JwtUtils;
 import com.gallendar.gradle.server.members.dto.LoginRequest;
 import com.gallendar.gradle.server.members.dto.LoginResponse;
+import com.gallendar.gradle.server.members.dto.MemberSearchResponse;
 import com.gallendar.gradle.server.members.service.LoginService;
-import com.gallendar.gradle.server.members.service.MemberService;
+import com.gallendar.gradle.server.members.service.MemberSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MembersController {
     private final LoginService loginService;
     private final JwtUtils jwtUtils;
+    private final MemberSearchService memberSearchService;
+
     @PostMapping("/authentication")
-    public LoginResponse membersLogin(@RequestBody LoginRequest loginRequest){
+    public LoginResponse membersLogin(@RequestBody LoginRequest loginRequest) {
         return loginService.LoginMembers(loginRequest);
+    }
+
+    @GetMapping("/members/search/{id}")
+    public List<MemberSearchResponse> searchMemberById(@PathVariable(value = "id") String id) {
+        return memberSearchService.MemberSearchById(id);
     }
 }
---
+++ server/src/main/java/com/gallendar/gradle/server/members/service/MemberSearchService.java
@@ -0,0 +1,21 @@
+package com.gallendar.gradle.server.members.service;
+
+import com.gallendar.gradle.server.members.domain.Members;
+import com.gallendar.gradle.server.members.domain.MembersRepositoryCustom;
+import com.gallendar.gradle.server.members.dto.MemberSearchResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class MemberSearchService {
+    private final MembersRepositoryCustom membersRepositoryCustom;
+
+    public List<MemberSearchResponse> MemberSearchById(String id) {
+        List<Members> members = membersRepositoryCustom.findByUser(id);
+        return members.stream().map(MemberSearchResponse::from).collect(Collectors.toList());
+    }
+}
