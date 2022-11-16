e3a2d12976fbae0ff657b80143a76625004b3820
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-11-16 14:32:57 +0900

    ♻️refactor : members entitiy

--- server/src/main/java/com/gallendar/gradle/server/members/domain/Members.java
+++ server/src/main/java/com/gallendar/gradle/server/members/domain/Members.java
@@ -2,11 +2,13 @@ package com.gallendar.gradle.server.members.domain;
 
 import com.gallendar.gradle.server.global.auditing.BaseTimeEntity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Members extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
