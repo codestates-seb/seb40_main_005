50975004d55b2c883df9284dda3e05fc4ef6f2c2
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-11-18 18:59:47 +0900

    ✨ feat : 알림 기능(현재 접속한 유저가 태그된 정보를 반환)

---
+++ server/src/main/java/com/gallendar/gradle/server/tags/domain/BoardTagsRepositoryCustom.java
@@ -0,0 +1,8 @@
+package com.gallendar.gradle.server.tags.domain;
+
+import com.gallendar.gradle.server.board.entity.Board;
+
+import java.util.List;
+
+public interface BoardTagsRepositoryCustom {
+}
