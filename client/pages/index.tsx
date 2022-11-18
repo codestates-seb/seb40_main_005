956903eeb249bca03d8ec2fa01ac9d6c4e404ffe
yeowool1010 <qyoong3579@gmail.com>
2022-11-18 15:36:44 +0900

    ✨  Feature/ yw/ 하단 스크롤 이미지, 움직임 구현/ #60

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,17 +1,11 @@
+import ScrollImg from "../components/scrollImg"
+
 function Home() {
   return (
     <>
-      <div className='bg-amber-500 font-SCDream1'>환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream2">환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream3">환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream4">환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream5">환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream6">환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream7">환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream8">환영합니다! 넥스트 제이에스</div>
-      <div className="bg-amber-500 font-SCDream9">환영합니다! 넥스트 제이에스</div>
-    </>
-    
+
+      <ScrollImg />
+    </> 
   )
   
   
