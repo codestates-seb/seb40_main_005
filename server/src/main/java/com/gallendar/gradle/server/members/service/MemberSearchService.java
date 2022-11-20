389477ccfbb691883cc045c1abbb7d8b92590d5a
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-11-21 01:35:04 +0900

    🐛  fixed : member search 코드에 오류가 있는 부분을 수정 (#151)

--- server/src/main/java/com/gallendar/gradle/server/members/service/MemberSearchService.java
+++ server/src/main/java/com/gallendar/gradle/server/members/service/MemberSearchService.java
@@ -1,7 +1,7 @@
 package com.gallendar.gradle.server.members.service;
 
 import com.gallendar.gradle.server.members.domain.Members;
-import com.gallendar.gradle.server.members.domain.MembersRepositoryCustom;
+import com.gallendar.gradle.server.members.domain.MembersRepositoryCustomImpl;
 import com.gallendar.gradle.server.members.dto.MemberSearchResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +12,11 @@ import java.util.stream.Collectors;
 @Service
 @AllArgsConstructor
 public class MemberSearchService {
-    private final MembersRepositoryCustom membersRepositoryCustom;
+    private final MembersRepositoryCustomImpl membersRepositoryCustom;
 
     public List<MemberSearchResponse> MemberSearchById(String id) {
         List<Members> members = membersRepositoryCustom.findByUser(id);
         return members.stream().map(MemberSearchResponse::from).collect(Collectors.toList());
+
     }
 }
