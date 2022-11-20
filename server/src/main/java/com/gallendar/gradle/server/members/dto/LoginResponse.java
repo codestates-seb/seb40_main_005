a3290e784a11af881dd470ac94cacd918140b2d6
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-11-20 23:07:56 +0900

    📈 add : swagger (#142)

--- server/src/main/java/com/gallendar/gradle/server/members/dto/LoginResponse.java
+++ server/src/main/java/com/gallendar/gradle/server/members/dto/LoginResponse.java
@@ -1,10 +1,12 @@
 package com.gallendar.gradle.server.members.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class LoginResponse {
+    @ApiModelProperty(value="토큰",dataType = "String")
     private String token;
 }
