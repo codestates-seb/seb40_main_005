91cd77d9ce5ca239501f23e47c7efb73429ce066
EUNBI <71588080+rachelyu1025@users.noreply.github.com>
2022-11-21 22:06:36 +0900

    ✨ Feature/eunbi/ index파일 및 Dots파일 수정#59 (#158)
    * 💄 Feature/ eunbi/ heroicons 설치
    
    * 💄 Feature/ eunbi/ 색상커스텀 코드 추가 
    
    * ✨ Feature/ eunbi / 랜딩페이지 스크롤 관련 작업(중)
    
    * 💄 Feature/ eunbi/ scrollImg z-index 값 수정
    
    * ✨ Feature/ eunbi / index파일 및 Dots 파일 수정

--- client/components/Dots.tsx
+++ client/components/Dots.tsx
@@ -1,6 +1,14 @@
 const Dot = ({ num, scrollIdx }: { num: number; scrollIdx: number }) => {
+  const handleScroll = () => {
+    window.scrollTo({
+      top: (document.getElementById(`${num}`) as HTMLDivElement).offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div
+      onClick={handleScroll}
       className={`${
         scrollIdx === num ? "bg-orange" : "bg-lightOrange"
       } duration-1000 w-3.5 h-3.5 border-solid rounded-full cursor-pointer`}
@@ -11,10 +19,10 @@ const Dot = ({ num, scrollIdx }: { num: number; scrollIdx: number }) => {
 function Dots({ scrollIdx }: { scrollIdx: number }) {
   return (
     <div className="fixed top-[45%] right-[30px] z-50">
-      <div className="z-50 flex flex-col items-center justify-between w-5 h-24">
+      <div className="flex flex-col items-center justify-between w-5 h-24">
         <Dot num={1} scrollIdx={scrollIdx}></Dot>
         <Dot num={2} scrollIdx={scrollIdx}></Dot>
-        <Dot num={3} scrollIdx={scrollIdx}></Dot>
+        <Dot num={6} scrollIdx={scrollIdx}></Dot>
       </div>
     </div>
   );
