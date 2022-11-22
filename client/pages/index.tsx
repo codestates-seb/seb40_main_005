4fd386330740528b1ea3716a81049fd535471ce9
hun0613 <106587166+hun0613@users.noreply.github.com>
2022-11-22 10:58:39 +0900

    🐛Feature/sunghun/인증페이지 회원가입구현#86 (#163)
    * ✨ Feature/sunghun/랜딩페이지 퍼블리싱 완료 #57
    
    * 🐛 fix/topBtn 버그 수정
    
    * 🐛 Fixed/랜딩페이지 모바일버전 버그 수정
    
    * 🐛 Fixed/사용설명페이지 스크롤 애니메이션 제거 및 폰트 굵기 수정#86

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -240,12 +240,14 @@ function Home() {
                 </div>
                 <div className="absolute top-5 left-0.5 right-0 bottom-0.5  bg-orange/40"></div>
               </div>
+              <Link href="/signUp">
               <div className="relative items-center justify-center cursor-pointer w-fit h-7">
                 <div className="z-10 ml-1 text-lg text-gray-700 font-SCDream3">
                   회원가입
                 </div>
                 <div className="absolute top-5 left-1 right-0 bottom-0.5  bg-orange/40"></div>
               </div>
+              </Link>
             </div>
           </div>
         </div>
