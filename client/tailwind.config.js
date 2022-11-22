20ae1745f6a20b900311ef0e1715747d60d1b14a
yeowool1010 <99955022+yeowool1010@users.noreply.github.com>
2022-11-22 11:54:10 +0900

    ✨ Feature/yw/tailwind.config.js색추가 및 인증페이지jsDoc작성 (#164)
    * ✨  Feature/ yw/ 인증페이지 재사용레이아웃 컴포넌트 구현/ #82
    
    * ✨ Feature/yw/tailwind.config색 추가 인증페이지 외곽구현중/ #82
    
    * ✨ Feature/yw/tailwind.config.js색추가 및 인증페이지jsDoc작성

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -26,6 +26,8 @@ module.exports = {
         lightOrange: "rgb(235 ,130 ,83 ,0.3)",
         orange: "#EB8253",
         bgGray: "#F8F9FD",
+        bgWhite: "#FEFEFE",
+
       },
       screens: {
         md: "376px",
