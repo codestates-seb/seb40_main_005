31805446db4017e884ed91d34282d58faceca5fd
rachelyu1025 <rachelyu.1025@gmail.com>
2022-11-23 22:00:53 +0900

    ✨ Feature / eunbi/ Btn 컴포넌트 스타일 수정 및 LoginForm 구현중

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,8 +1,26 @@
+import { LoginForm } from "../components/LoginForm";
+import CertifyPageLayout from "../components/CertifyPageLayout";
+import GoogleBtn from "../components/GoogleBtn";
+import KakaoBtn from "../components/KakaoBtn";
+import SubmitBtn from "../components/SubmitBtn";
+
 const Login = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <>
+      <CertifyPageLayout>
+        <div className="flex flex-col h-[75%] md:h-[85%] lg:h-full justify-between">
+          <LoginForm />
+          <div className="flex justify-end">
+            <SubmitBtn />
+          </div>
+          <div className="flex flex-col md:items-center">
+            <GoogleBtn />
+            <KakaoBtn />
+          </div>
+        </div>
+      </CertifyPageLayout>
+    </>
+  );
+};
 
 export default Login;
