795a6e9c513a879f89f84868fd70c707555f4538
hun0613 <pshun7614@gmail.com>
2022-11-23 23:04:59 +0900

    🐛 Fiexed/sunghun/getUserId파일 삭제

--- client/apis/user/getUserId.tsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const fetchIdCheck = (userid : string) => { // 요청메소드 + 요청정보
-    return axios.get(`http://13.209.7.184:8080/members/search?id=${userid}`);
-}
-
-export default fetchIdCheck;
