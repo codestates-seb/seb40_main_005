79ab94d5a5cba484857f0a9c79db24dddb23447c
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-11-25 01:52:50 +0900

    ♻️ refactor : passwordEncoder Component

--- server/src/main/java/com/gallendar/gradle/server/members/service/LoginService.java
+++ server/src/main/java/com/gallendar/gradle/server/members/service/LoginService.java
@@ -1,12 +1,12 @@
 package com.gallendar.gradle.server.members.service;
 
+import com.gallendar.gradle.server.global.auth.encoder.CommonEncoder;
 import com.gallendar.gradle.server.global.auth.jwt.JwtUtils;
 import com.gallendar.gradle.server.members.domain.Members;
 import com.gallendar.gradle.server.members.domain.MembersRepository;
 import com.gallendar.gradle.server.members.dto.LoginRequest;
 import com.gallendar.gradle.server.members.dto.LoginResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,16 +14,14 @@ import org.springframework.stereotype.Service;
 public class LoginService {
     private final JwtUtils jwtUtils;
     private final MembersRepository membersRepository;
-    PasswordEncoder passwordEncoder;
+    private final CommonEncoder commonEncoder;
 
-    public LoginResponse LoginMembers(LoginRequest loginRequest){
-        final String memberId = loginRequest.getId();
-        final String password = loginRequest.getPassword();
-        Members members=membersRepository.findById(memberId).orElseThrow(()->new IllegalArgumentException("아이디 또는 비밀번호를 확인해주세요."));
-        if(!passwordEncoder.matches(password,members.getPassword())){
+    public LoginResponse LoginMembers(LoginRequest loginRequest) {
+        Members members = membersRepository.findById(loginRequest.getId()).orElseThrow(() -> new IllegalArgumentException("아이디 또는 비밀번호를 확인해주세요."));
+        if (!commonEncoder.matches(loginRequest.getPassword(), members.getPassword())) {
             throw new IllegalArgumentException("아이디 또는 비밀번호를 확인해주세요.");
         }
-        String token=jwtUtils.generateToken(memberId);
-        return new LoginResponse(token,members.getMembersId(),members.getId());
+        String token = jwtUtils.generateToken(loginRequest.getId());
+        return new LoginResponse(token, members.getMembersId(), members.getId());
     }
 }
