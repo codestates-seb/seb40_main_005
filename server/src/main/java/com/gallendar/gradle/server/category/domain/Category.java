0bea5722689648b67ac79dbae9e6c268805f2e68
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-11-24 17:32:16 +0900

    Feature/dg/board search#201 (#201)
    * ✨ feat : 게시글 조회 api
    
    * ✅ add : board search test

--- server/src/main/java/com/gallendar/gradle/server/category/domain/Category.java
+++ server/src/main/java/com/gallendar/gradle/server/category/domain/Category.java
@@ -1,10 +1,12 @@
 package com.gallendar.gradle.server.category.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import lombok.Getter;
+
+import javax.persistence.*;
 
 @Entity
+@Getter
 public class Category {
     @Id
     @Column(name = "category_id")
