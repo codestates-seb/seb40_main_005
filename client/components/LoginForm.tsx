2e2c7e52199eecace719586d694683a33c17dd3a
rachelyu1025 <rachelyu.1025@gmail.com>
2022-11-24 23:00:25 +0900

    🐛  fixed / eunbi / 병합 충돌 해결 후 파일 재수정

--- client/components/LoginForm.tsx
+++ client/components/LoginForm.tsx
@@ -2,8 +2,8 @@ import { useSetRecoilState } from "recoil";
 import { isLoginState } from "../recoil/authAtom";
 import { useForm } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
-import { Dispatch, SetStateAction, useState } from "react";
 import Link from "next/link";
+import { Dispatch, SetStateAction, useState } from "react";
 import postLogin from "../apis/auth/postLogin";
 import { useMutation } from "react-query";
 import { useRouter } from "next/router";
@@ -37,7 +37,7 @@ const LoginForm = () => {
           localStorage.setItem("token", res.token);
           setIsLogin(true);
           setFailedMsg("");
-          router.push("/signup");
+          router.push("/main");
         }
       })
       .catch(() => {
@@ -69,19 +69,23 @@ const LoginForm = () => {
           <div className="absolute w-4 h-2 top-[0.8rem] md:top-3 lg:w-4 lg:top-[0.8rem] bg-mainOrange/40"></div>
           <input
             className="mt-4 text-sm outline-none font-SCDream3 border-b-[1px] border-mainOrange/40"
-            id="loginId"
+            id="id"
+            placeholder="ID를 입력 후 ENTER를 눌러주세요"
             type="text"
             {...register("id", {
               required: "ID를 입력해주세요!",
               onChange: handleIdChange,
               minLength: 5,
+              pattern: {
+                value: /^[a-zA-Z0-9]{5,20}$/,
+                message: "ID는 영문,숫자 5자 이상 입력되어야합니다.",
+              },
             })}
-            placeholder="ID를 입력 후 ENTER를 눌러주세요"
-          ></input>
+          />
 
           <ErrorMessage
             errors={errors}
-            name="singleErrorInput"
+            name="id"
             render={({ message }) => (
               <div className="flex flex-row items-end justify-end w-full mt-1 text-xs text-nagativeMessage h-fit font-SCDream2">
                 {message}
@@ -110,8 +114,11 @@ const LoginForm = () => {
             type="password"
             {...register("password", {
               required: "비밀번호를 입력해주세요!",
-              pattern:
-                /^(?=.[A-Za-z])(?=.\d)(?=.[$@$!%#?&])[A-Za-z\d$@$!%*#?&]{8,25}$/,
+              pattern: {
+                value:
+                  /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,25}$/,
+                message: "비밀번호 형식에 맞게 입력해주세요",
+              },
             })}
           />
 
@@ -137,6 +144,10 @@ const LoginForm = () => {
               <Link href={"/signup"}>비밀번호재설정</Link>
             </div>
           </div>
+          <input
+            type="submit"
+            className="self-end w-20 mt-3 md:w-28 h-8 rounded-2xl bg-btnOrange text-white font-SCDream5 text-sm hover:bg-[#fcb79a]"
+          ></input>
         </form>
 
         {/* 로그인 실패 메세지 */}
