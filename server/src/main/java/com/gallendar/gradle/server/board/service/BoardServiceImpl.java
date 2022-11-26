b30d6dda27d2277a8c68a31dd00b3fd79ad38c8d
JungSoyi <soi0125@naver.com>
2022-11-27 03:54:30 +0900

    :zap: improve : tags board mapping

--- server/src/main/java/com/gallendar/gradle/server/board/service/BoardServiceImpl.java
+++ server/src/main/java/com/gallendar/gradle/server/board/service/BoardServiceImpl.java
@@ -7,6 +7,14 @@ import com.gallendar.gradle.server.board.dto.BoardUpdateRequestDto;
 import com.gallendar.gradle.server.board.entity.Board;
 import com.gallendar.gradle.server.board.repository.BoardRepository;
 import com.gallendar.gradle.server.members.domain.Members;
+import com.gallendar.gradle.server.members.domain.MembersRepository;
+import com.gallendar.gradle.server.photo.entity.Photo;
+import com.gallendar.gradle.server.photo.repository.PhotoRepository;
+import com.gallendar.gradle.server.photo.service.S3UploadService;
+import com.gallendar.gradle.server.tags.domain.*;
+import com.gallendar.gradle.server.tags.dto.TagsCreateDto;
+import com.gallendar.gradle.server.tags.domain.BoardTags;
+import com.gallendar.gradle.server.tags.type.TagStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,8 +22,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.function.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,11 +34,47 @@ import java.util.stream.Collectors;
 public class BoardServiceImpl implements BoardService{
 
     private final BoardRepository boardRepository;
+    private final MembersRepository membersRepository;
+    private final S3UploadService photoService;
+    private final PhotoRepository photoRepository;
+    private final BoardTagsRepository boardTagsRepository;
+    private final TagsRepository tagsRepository;
 
     /* 게시글 저장 */
     @Transactional
-    public Long save(BoardCreateRequestDto requestDto){
-        return boardRepository.save(requestDto.toEntity()).getBoardId();
+    public Long save(BoardCreateRequestDto requestDto, List<String> tagsMembers) throws IOException {
+
+        String fileName= UUID.randomUUID()+"-"+requestDto.getPhoto().getOriginalFilename();
+        String path = photoService.upload(requestDto.getPhoto());
+        Photo photo = Photo.builder().fileName(fileName).path(path).build();
+
+        Members member = membersRepository.findById(requestDto.getMemberId())
+                .orElseThrow(() -> new IllegalArgumentException());
+
+        Board board = requestDto.toEntity();
+
+        board.setMembers(member);
+        board.setPhoto(photo);
+        boardRepository.save(board);
+        photoRepository.save(photo);
+
+
+        tagsMembers.forEach(m -> {
+
+            BoardTags boardTags = new BoardTags();
+            Tags tags = Tags.builder()
+                    .tagsMember(m)
+                    .tagStatus(TagStatus.alert)
+                    .build();
+            boardTags.setBoard(board);
+            boardTags.setTags(tags);
+
+            boardTagsRepository.save(boardTags);
+            tagsRepository.save(tags);
+
+        });
+            return null;
+
     }
     /* 게시글 수정 */
     @Transactional
@@ -41,6 +88,8 @@ public class BoardServiceImpl implements BoardService{
                 .ifPresent(content->requestDto.setContent(content));
         Optional.ofNullable(requestDto.getMusic())
                 .ifPresent(music->requestDto.setMusic(music));
+        Optional.ofNullable(requestDto.getPhoto())
+                .ifPresent(photo->requestDto.setPhoto(photo));
 
         return boardId;
     }
@@ -82,7 +131,7 @@ public class BoardServiceImpl implements BoardService{
      * Todo:
      * ExceptionCode 작성후 error response 수정
      */
-    private void isAutoorized(Board board, Members members){
+    private void isAuthorized(Board board, Members members){
         if(!board.getMembers().equals(members)) {
             throw new IllegalArgumentException("사용자가 일치하지 않습니다.");
         }
