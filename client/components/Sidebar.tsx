c31af6b631beecb955b4bb58772b878239fa8bbe
yeowool1010 <qyoong3579@gmail.com>
2022-11-28 21:47:08 +0900

    ✨ Feature/ yw/ 카테고리 태그 레이아웃 구현 /#65

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -1,7 +1,22 @@
-import { BuildingStorefrontIcon } from '@heroicons/react/24/solid'
-import Link from 'next/link';
+import { BuildingStorefrontIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { handleClientScriptLoad } from "next/script";
 
-const Sidebar = () => {
+import SidebarCategory from "./SidebarCategory";
+import React, { useEffect } from "react";
+// import dummy from "../dummy";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Sidebar = ({ children }: Props) => {
+  const handleTagClick = () => {
+    console.log("카테고리클릭");
+    const point = document.getElementById("point");
+    // point.style.display = "none";
+  };
+
   return (
     <>
       <section className="flex flex-col justify-center lg:justify-around items-center w-full lg:w-1/6 h-full pr-4 lg:pr-0 pl-4 pt-14">
@@ -12,12 +27,18 @@ const Sidebar = () => {
           <div className="absolute top-4 lg:top-5 left-0.5 right-0 bottom-1.5 lg:bottom-0.5  bg-mainOrange/40"></div>
         </div>
 
-        <div className="w-full h-1/3 lg:h-2/3 bg-slate-200 rounded-md overflow-auto"></div>
+        <div className="grid grid-cols-1 text-xs drop-shadow-xl lg:flex lg:flex-col w-full h-1/3 lg:h-5/6 font-SCDream4 overflow-auto">
+          {/* {dummy.map(dummy => (
+            <SidebarCategory onClick={handleTagClick} key={dummy.boardId}>
+              <div>{dummy.content}</div>
+            </SidebarCategory>
+          ))} */}
+        </div>
 
         <Link href="/commerce">
           <div className="flex flex-row justify-center items-center w-fit h-fit cursor-pointer mt-10 lg:mt-0">
             <span className="w-fit h-fit bg-mainOrange p-1 rounded-lg mr-1">
-              <BuildingStorefrontIcon className='w-4 h-4 text-white'/>
+              <BuildingStorefrontIcon className="w-4 h-4 text-white" />
             </span>
             <div className="relative items-center justify-center w-fit h-7 mt-1.5">
               <div className="z-10 ml-0.5 text-sm md:text-sm lg:text-sm text-gray-700 font-SCDream3">
