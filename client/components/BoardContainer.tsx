c0a45e9bc5ad75c602214cd42b5c7f625b3377f2
hun0613 <pshun7614@gmail.com>
2022-11-28 21:27:17 +0900

    ✨ Feature/sunghun/게시글작성모달 80%구현 완료 및 브랜치 코드 동기화를 위한 commit

--- client/components/BoardContainer.tsx
+++ client/components/BoardContainer.tsx
@@ -7,7 +7,7 @@ const BoardContainer = ({ children, boardRef }: Props) => {
   return (
     <>
       <div
-        className="p-5 rounded-xl right-0 drop-shadow-xl fixed h-[90%] md:h-screen w-full md:w-[700px] bg-bgWhite/90 z-10 md:translate-x-full transition ease-in-out duration-[700ms]"
+        className="p-5 rounded-xl right-0 drop-shadow-xl fixed h-[90%] md:h-screen w-full md:w-[700px] bg-bgWhite/90 z-10 translate-x-full transition ease-in-out duration-[700ms]"
         ref={boardRef}
       >
         {children}
