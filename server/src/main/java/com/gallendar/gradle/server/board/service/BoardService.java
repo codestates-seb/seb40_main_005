398274a9eade81c12851aa78d5f53766f20f23be
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-11-29 13:34:11 +0900

    🔥 delete

--- server/src/main/java/com/gallendar/gradle/server/board/service/BoardService.java
+++ server/src/main/java/com/gallendar/gradle/server/board/service/BoardService.java
@@ -1,17 +1,9 @@
 package com.gallendar.gradle.server.board.service;
 
 import com.gallendar.gradle.server.board.dto.BoardCreateRequestDto;
-import com.gallendar.gradle.server.board.dto.BoardListResponseDto;
-import com.gallendar.gradle.server.board.dto.BoardResponseDto;
 import com.gallendar.gradle.server.board.dto.BoardUpdateRequestDto;
-import com.gallendar.gradle.server.board.entity.Board;
-import com.gallendar.gradle.server.category.domain.Category;
-import com.gallendar.gradle.server.members.domain.Members;
-import com.gallendar.gradle.server.tags.dto.TagsCreateDto;
-import org.springframework.data.domain.Page;
 
 import java.io.IOException;
-import java.util.List;
 
 public interface BoardService {
     void save(BoardCreateRequestDto requestDto, String token) throws IOException;
