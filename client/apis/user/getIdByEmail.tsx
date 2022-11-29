b8cb7159c680d1050cffd3a2f78f2886d0b86d27
yeowool1010 <qyoong3579@gmail.com>
2022-11-29 17:35:25 +0900

    ✨ Feature/ yw/ 카테고리 api연결중 / #65

--- client/apis/user/getIdByEmail.tsx
+++ client/apis/user/getIdByEmail.tsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 /**
- * @description userEmail을 props로 받아서 id를 return하는 hook
+ * @author yeowool
+ * @description userEmail을 props로 받아서 id를 return
  **/
 
 const fetchIdByEmail = (userEmail: string) => {
   // 요청메소드 + 요청정보
   return axios.get(
     `${process.env.NEXT_PUBLIC_API_URL}/members/find-id?email=${userEmail}`,
-    // `http://13.209.7.184:8080/members/${userEmail}`,
-    // "http://13.209.7.184:8080/members/find-id?email=yw1010@naver.com"
   );
 };
 
