a20e4377b2237248fb49d5af3a6cf6887b2c918e
JungSoyi <104415354+JungSoyi@users.noreply.github.com>
2022-11-29 17:01:06 +0900

    :construction_worker: CI/CD 위한 deploy.sh 파일 생성 (#268)

---
+++ server/$buildDir/generated/querydsl/com/gallendar/gradle/server/category/domain/QCategory.java
@@ -0,0 +1,39 @@
+package com.gallendar.gradle.server.category.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QCategory is a Querydsl query type for Category
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCategory extends EntityPathBase<Category> {
+
+    private static final long serialVersionUID = 509259845L;
+
+    public static final QCategory category = new QCategory("category");
+
+    public final NumberPath<Long> categoryId = createNumber("categoryId", Long.class);
+
+    public final StringPath categoryTitle = createString("categoryTitle");
+
+    public QCategory(String variable) {
+        super(Category.class, forVariable(variable));
+    }
+
+    public QCategory(Path<? extends Category> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QCategory(PathMetadata metadata) {
+        super(Category.class, metadata);
+    }
+
+}
+
