e077cf20da81a545c8d2e3e4e87a099611f8bf6d
EUNBI <71588080+rachelyu1025@users.noreply.github.com>
2022-11-30 11:31:20 +0900

    ✨ Feature / eunbi/ 게시글타이틀 컨테이너 구현 (#279)

---
+++ client/components/MyBoard.tsx
@@ -0,0 +1,14 @@
+const MyBoard = () => {
+  return (
+    <>
+      <div onClick={() => {}} className="flex flex-row pt-2">
+        <div className="w-5 h-2 rounded-full md:w-[0.15rem] lg:w-[0.18rem] md:h-[0.85rem] lg:h-4 md:rounded-none bg-coral"></div>
+        <span className="truncate md:w-24 lg:w-30 hidden md:inline-block ml-2 md:text-[0.65rem] lg:text-sm text-textGray font-SCDream5 overflow-ellipsis">
+          제주도제주도제주도{/* board.title */}
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default MyBoard;
