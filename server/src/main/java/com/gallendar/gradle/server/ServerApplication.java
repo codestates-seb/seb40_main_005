b1850b8de5d8ac2d2cfa8766cf9000493859edaa
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-12-02 03:36:03 +0900

    📝 fixed (#313)

--- server/src/main/java/com/gallendar/gradle/server/ServerApplication.java
+++ server/src/main/java/com/gallendar/gradle/server/ServerApplication.java
@@ -9,6 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication(exclude = ContextRegionProviderAutoConfiguration.class)
 @EnableJpaAuditing
 public class ServerApplication {
+
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
