375a66b32df3d3ce3eb4c50300faffd5daa0a965
DG LEE <81510864+riverrevir@users.noreply.github.com>
2022-12-02 03:25:27 +0900

    Feature/dg/s3 (#312)
    * 📝 fixed : s3
    
    * fixed

--- server/src/main/java/com/gallendar/gradle/server/ServerApplication.java
+++ server/src/main/java/com/gallendar/gradle/server/ServerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication(exclude = ContextRegionProviderAutoConfiguration.class)
 @EnableJpaAuditing
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
