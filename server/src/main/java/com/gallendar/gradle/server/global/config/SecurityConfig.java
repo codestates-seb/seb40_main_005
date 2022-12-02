6837495220c92162f37b3f638aadcf84f8eb4afe
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-12-02 20:01:50 +0900

    🐛 fixed : security error

--- server/src/main/java/com/gallendar/gradle/server/global/config/SecurityConfig.java
+++ server/src/main/java/com/gallendar/gradle/server/global/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.gallendar.gradle.server.global.config;
 
-
 import com.gallendar.gradle.server.global.auth.jwt.JwtAuthenticationEntryPoint;
 import com.gallendar.gradle.server.global.auth.jwt.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -21,30 +20,28 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
+
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
-
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors().configurationSource(corsConfigurationSource())
                 .and()
                 .csrf().disable()
                 // 이 요청은 인증을 하지 않는다.
-                .authorizeRequests()
-                .antMatchers(
-
-
+                .authorizeRequests().antMatchers(
                         "/authentication/**"
                         , "/members/**"
                         // -- Swagger UI v2
@@ -52,8 +49,6 @@ public class SecurityConfig {
                         , "/swagger-ui.html", "/webjars/**", "/swagger/**"
                         // -- Swagger UI v3 (Open API)
                         , "/v3/api-docs/**", "/swagger-ui/**")
-
-
                 .permitAll()
                 // 다른 모든 요청은 인증을 한다.
                 .anyRequest().authenticated().and()
@@ -62,15 +57,14 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         // 모든 요청에 토큰을 검증하는 필터를 추가한다.
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
@@ -79,3 +73,4 @@ public class SecurityConfig {
         return source;
     }
 }
+
