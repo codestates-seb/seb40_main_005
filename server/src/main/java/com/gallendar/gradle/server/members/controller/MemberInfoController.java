7ed027c4c3bad8455139e068065b2b9c99bdf68f
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-12-04 00:27:53 +0900

    🎨 fixed : response page

--- server/src/main/java/com/gallendar/gradle/server/members/controller/MemberInfoController.java
+++ server/src/main/java/com/gallendar/gradle/server/members/controller/MemberInfoController.java
@@ -8,10 +8,10 @@ import com.gallendar.gradle.server.members.service.MemberInfoService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,7 +43,7 @@ public class MemberInfoController {
      */
     @ApiOperation(value = "태그 로그 조회", notes = "누가 누구한테 공유 했는지와 상태를 반환")
     @GetMapping("/tag")
-    public List<MemberTagStatusResponse> getMySharedStatusById(@RequestHeader(value = JwtRequestFilter.HEADER_KEY) String token, MemberTagStatusRequest memberTagStatusRequest, Pageable pageable) {
+    public Page<MemberTagStatusResponse> getMySharedStatusById(@RequestHeader(value = JwtRequestFilter.HEADER_KEY) String token, MemberTagStatusRequest memberTagStatusRequest, Pageable pageable) {
         log.info("태그 로그 조회 요청");
         return memberInfoService.mySharedStatusGetById(token, memberTagStatusRequest, pageable);
     }
