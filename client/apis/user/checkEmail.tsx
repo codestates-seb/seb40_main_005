85087cc6163e84bb839a0af4d1ffaae5ddf9c036
EUNBI <71588080+rachelyu1025@users.noreply.github.com>
2022-12-05 18:05:18 +0900

    🐛 fixed/ eunbi / 인증페이지 관련 api 요청 함수 헤더수정#100 (#365)
    * 🐛 fixed / eunbi / 공유알림  기능 수정 및 알림 레이아웃 수정
    
    * 🐛  fixed/ eunbi / 인증페이지 관련 api 요청 함수 헤더수정

--- client/apis/user/checkEmail.tsx
+++ client/apis/user/checkEmail.tsx
@@ -3,9 +3,17 @@ import client from "../../client/client";
 
 const checkEmail = async (email: string) => {
   // 요청메소드 + 요청정보
-  return await axios.get(`/members/checkEmail/${email}`).catch(err => {
-    return err;
-  });
+  return await axios
+    .get(`/members/checkEmail/${email}`, {
+      baseURL: process.env.NEXT_PUBLIC_API_URL,
+      headers: {
+        withCredentials: true,
+        "Content-Type": `application/json`,
+      },
+    })
+    .catch(err => {
+      return err;
+    });
 };
 
 export default checkEmail;
--- client/apis/user/checkUserId.tsx
+++ client/apis/user/checkUserId.tsx
@@ -3,9 +3,17 @@ import client from "../../client/client";
 
 const checkUserId = async (userid: string) => {
   // 요청메소드 + 요청정보
-  return await axios.get(`/members/checkId/${userid}`).catch(err => {
-    return err;
-  });
+  return await axios
+    .get(`/members/checkId/${userid}`, {
+      baseURL: process.env.NEXT_PUBLIC_API_URL,
+      headers: {
+        withCredentials: true,
+        "Content-Type": `application/json`,
+      },
+    })
+    .catch(err => {
+      return err;
+    });
 };
 
 export default checkUserId;
